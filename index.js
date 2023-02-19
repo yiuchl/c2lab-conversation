290a785fd6e663469b5cd64e9c77ccc0d4b03ad1
yiuchl <yiuc318@newschool.edu>
2023-02-18 20:52:15 -0500

    Whole lotta editing

---
+++ index.js
@@ -0,0 +1,20 @@
+// let options = {
+//     root: document.querySelector('#scrollArea'),
+//     rootMargin: '0px',
+//     threshold: 1.0
+// }
+
+// let target = document.querySelector('.first-item');
+
+// let callback = (entries, observer) => {
+//     entries.forEach((entry) => {
+//         if(entry.isIntersecting){
+//             target.classList.add('visible')
+//         }
+//     });
+// };
+
+// let observer = new IntersectionObserver(callback, options);
+
+
+// observer.observe(target);
